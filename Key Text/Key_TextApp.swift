84459cb1e4255dcd5794bb93fcbafabea867cd9e
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2024-03-28 14:58:58 +0100

    Initial Commit

---
+++ Key Text/Key_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Key_TextApp.swift
+//  Key Text
+//
+//  Created by Kendall Felder on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Key_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
